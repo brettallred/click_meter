0e7b293a8e22cb4e5e4f85425157eeff0def884e
Brett Allred <brettallred@gmail.com>
2015-02-24 19:42:21 -0700

    Add comments to account

--- lib/click_meter/account.rb
+++ lib/click_meter/account.rb
@@ -1,4 +1,20 @@
 class ClickMeter::Account < ClickMeter::Base
+  # Attributes
+  # :email            => "brett@nuvi.com",
+  # :firstName        => "Brett",
+  # :registrationDate => "20150216140408",
+  # :timezone         => 82,
+  # :timezonename     => "Mountain Standard Time",
+  # :boGoVal          => "LRQEtv8QYDvPbn41NZNvNA%3d%3d"
+
+
+  # Retrieves the account data based on the Auth Key
+  #
+  # Response are the attributes above
   get :take, "/account"
+
+  # Retrieves the account data based on the Auth Key
+  #
+  # Any of the attributes above will be saved
   post :save, '/account'
 end
