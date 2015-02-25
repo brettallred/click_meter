294561784cf77b95d8a0e14dc3977e74461b589c
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-25 18:55:38 +0200

    update docs

--- lib/click_meter.rb
+++ lib/click_meter.rb
@@ -3,6 +3,8 @@ require "click_meter/datapoint"
 require "click_meter/version"
 require "click_meter/account"
 require "click_meter/account_plan"
+require "click_meter/conversion"
+require "click_meter/domain"
 require "active_rest_client"
 
 module ClickMeter
--- lib/click_meter/domain.rb
+++ lib/click_meter/domain.rb
@@ -1,8 +1,45 @@
 class ClickMeter::Domain < ClickMeter::Base
+  #Create a new personal domain to use in your datapoints
+  # Response:
+  # :id   => "integer"
+  # :uri  => "string"
+  # :name => "string"
+  #
   post :create, '/domains'
+
+  #List domains to which is possible to register a datapoint
+  # Response
+  # {
+  #  :entities => [
+  #    {:id => 123, :uri => "string"}, {:id => 321, :uri => "string"}
+  #  ]
+  #}
   get :all, '/domains',
+
+  #Delete a domain from listing
+  # Response
+  # :id => 7711
+  # :uri => "string"
   delete :destroy, '/domains/:id'
+
+  #Get the detail about a domain
+  # Response:
+  # :id   => "integer"
+  # :uri  => "string"
+  # :name => "string"
   get :find, '/domains/:id'
+
+  #Update a domain object
+  # Response:
+  # :id   => "integer"
+  # :uri  => "string"
+  # :name => "string"
   post :save, '/domains/:id'
+
+  #Get a count of domains available
+  # Response:
+  # {
+  #   :count => "integer"
+  # }
   get :count, '/domains/count'
 end
