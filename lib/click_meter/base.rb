fb9cc6aa67a0ae26b4467eba7b42ca6fefd7dbec
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-25 18:57:22 +0200

    reformat code

--- lib/click_meter/base.rb
+++ lib/click_meter/base.rb
@@ -5,13 +5,13 @@ module ClickMeter
   class Base < ActiveRestClient::Base
     include Configuration
 
-  	before_request :add_authentication_details
+    before_request :add_authentication_details
     request_body_type :json
 
-  	private
+    private
 
-  	def add_authentication_details(name, request)
-  		request.headers["X-Clickmeter-Authkey"] = self.class.auth_key.to_s
-  	end
+    def add_authentication_details(name, request)
+      request.headers["X-Clickmeter-Authkey"] = self.class.auth_key.to_s
+    end
   end
 end
