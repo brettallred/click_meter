8e1b76780c2c4bdce03c103e46ffdc3944083105
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-26 17:30:50 +0200

    make all response examples be in same format

--- lib/click_meter/account.rb
+++ lib/click_meter/account.rb
@@ -1,21 +1,19 @@
 #Seciton: Accounting
 class ClickMeter::Account < ClickMeter::Base
-  # Attributes
-  # :email            => "brett@nuvi.com",
-  # :firstName        => "Brett",
-  # :registrationDate => "20150216140408",
-  # :timezone         => 82,
-  # :timezonename     => "Mountain Standard Time",
-  # :boGoVal          => "LRQEtv8QYDvPbn41NZNvNA%3d%3d"
+  #Get details of the current logged account
+  # Response
+  # {
+  #     "email": "user@example.com",
+  #     "firstName": "User",
+  #     "registrationDate": "20150216140408",
+  #     "timezone": 82,
+  #     "timezonename": "Mountain Standard Time",
+  #     "boGoVal": "LRQVtv9QYDvPbn41NZNvNA%3d%3d"
+  # }
 
-
-  # Retrieves the account data based on the Auth Key
-  #
-  # Response are the attributes above
   get :take, "/account"
 
-  # Retrieves the account data based on the Auth Key
-  #
-  # Any of the attributes above will be saved
+  #Update your account data
+  # Response: empty string
   post :save, '/account'
 end
