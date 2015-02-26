10de5fdbbe59c3f74bf483fbb3e1937819b922c9
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-26 13:51:54 +0200

    extend gem's configuration; add :debug key; add :debug_response method

--- lib/click_meter/base.rb
+++ lib/click_meter/base.rb
@@ -8,7 +8,20 @@ module ClickMeter
     before_request :add_authentication_details
     request_body_type :json
 
+    def self.inherited(klass)
+      super
+      klass.class_eval {
+        after_request :debug_response
+      }
+    end
+
     private
+
+    def debug_response(method, response)
+      return unless self.class.debug?
+      puts "Status: #{response.status}"
+      puts response.body
+    end
 
     def add_authentication_details(name, request)
       request.headers["X-Clickmeter-Authkey"] = self.class.auth_key.to_s
