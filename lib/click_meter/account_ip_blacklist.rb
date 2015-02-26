102ba54f378417e945a0ec0f7db3721e83306b2a
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-26 20:55:44 +0200

    implement Accounting - Blacklist methods

--- lib/click_meter/account_ip_blacklist.rb
+++ lib/click_meter/account_ip_blacklist.rb
@@ -1,1 +1,14 @@
 #Section: Accounting - Whitelist
+class ClickMeter::AccountIpBlacklist < ClickMeter::Base
+  #List custom blacklisted IPs. Valid only for master key. Maximum 50 elements
+  get :all, '/account/ipblacklist'
+
+  #Get information about a IP in blacklist. Valid only for master key
+  get :find, '/account/ipblacklist/:hash'
+
+  #Add a new IP in the blacklist. Valid only for master key. Maximum 50 elements
+  post :create, '/account/ipblacklist'
+
+  #Delete an ip in blacklist. Valid only for master key
+  delete :destroy, '/account/ipblacklist/:hash'
+end
