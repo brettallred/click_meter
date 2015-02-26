450be76a551fea0528f32f55965b0d53fe20b544
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-26 15:08:48 +0200

    fix :find method's url; update response examples

--- lib/click_meter/account_guest.rb
+++ lib/click_meter/account_guest.rb
@@ -1,27 +1,35 @@
 class ClickMeter::AccountGuest < ClickMeter::Base
   #Guest list account. Access only for master key.
   # Response:
-  # {"entities"=>[
-  #      {"id"=>7709, "uri"=>"string"},
-  #     {"id"=>7710, "uri"=>"string"}
-  #   ]
+  # {
+  #     "entities": [
+  #       {
+  #           "id": 258148,
+  #       "uri": "/account/guests/258148"
+  #       }
+  #     ]
   # }
   get :all, '/account/guests'
 
   #Guest detail. Access only for master key
   #Response
-  #{
-  #     "apikey": "string",
-  #     "id": "integer",
-  #     "name": "string",
-  #     "password": "string",
-  #     "timeZone": "integer",
-  #     "dateFormat": "string",
-  #     "decimalSeparator": "string",
-  #     "language": "string",
-  #     "numberGroupSeparator": "string"
-  #}
-  get :find, '/account/guest/:id'
+  # {
+  #     "apiKey": "698508de-83cc-4cb1-a83f-bb9ba55706cc",
+  #     "id": 258148,
+  #     "name": "User",
+  #     "password": "pxccsb",
+  #     "email": "user@example.com",
+  #     "timeZone": 82,
+  #     "notes": "",
+  #     "key": "ikJeRt/U2PttGYQcroYThw==",
+  #     "language": "en-US",
+  #     "dateFormat": "dd/MM/yyyy",
+  #     "timeFormat": 1,
+  #     "decimalSeparator": ".",
+  #     "numberGroupSeparator": ",",
+  #     "userName": "user"
+  # }
+  get :find, '/account/guests/:id'
 
   #Delete a guest
   delete :destroy, '/account/guest/:id'
