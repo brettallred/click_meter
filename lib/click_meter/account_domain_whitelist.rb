e58b8422d48a5e9b9d609436c1ffdc3d6f2d080d
Ivan Dzyzenko <ivan.dzyzenko@gmail.com>
2015-02-27 15:47:22 +0200

    According to the Francesco, '/account/ipblacklist/:id' and '/account/domainwhitelist/:id' is no longer supported

--- lib/click_meter/account_domain_whitelist.rb
+++ lib/click_meter/account_domain_whitelist.rb
@@ -3,9 +3,6 @@ class ClickMeter::AccountDomainWhitelist < ClickMeter::Base
   #List domains whitelist for this account. Valid only for master key. Maximum 50 elements.
   get :all, '/account/domainwhitelist'
 
-  #Get the detail for a domain in whitelist. Valid only for master key
-  get :find, '/account/domainwhitelist/:hash'
-
   #Add a new domain in whitelist. Valid only for master key. Maximum 50 elements.
   post :create, '/account/domainwhitelist'
 
--- lib/click_meter/account_ip_blacklist.rb
+++ lib/click_meter/account_ip_blacklist.rb
@@ -8,9 +8,6 @@ class ClickMeter::AccountIpBlacklist < ClickMeter::Base
   # }
   get :all, '/account/ipblacklist'
 
-  #Get information about a IP in blacklist. Valid only for master key
-  get :find, '/account/ipblacklist/:hash'
-
   #Add a new IP in the blacklist. Valid only for master key. Maximum 50 elements
   post :create, '/account/ipblacklist'
 
